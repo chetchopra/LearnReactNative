a6edb3f8ac4045074da96db0d3cdd6119df12723
chetchopra <chetanya.chopra94@gmail.com>
2019-08-22 03:08:04 -0700

    Its flips the whiteboard cards

--- screens/Whiteboard.js
+++ screens/Whiteboard.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
-import { Text } from 'react-native'
+import CardFlip from 'react-native-card-flip';
+
+import { 
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+ } from 'react-native'
 
 
 
@@ -8,7 +14,34 @@ import { Text } from 'react-native'
 export default class Whiteboard extends Component{
   render() {
     return (
-      <Text>gdfshgjdfsk</Text>
+      <CardFlip style={styles.cardContainer} ref={(card) => this.card = card} >
+        <TouchableOpacity style={styles.card} onPress={() => this.card.flip()} ><Text>QUESTION</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.card} onPress={() => this.card.flip()} ><Text>ANSWER</Text></TouchableOpacity>
+      </CardFlip>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    height: '100%',
+    width: '100%',
+    marginLeft: 'auto',
+    backgroundColor: '#565656',
+  },
+  card: {
+    height: '90%',
+    width: '90%',
+    backgroundColor: '#C09F80',
+    borderColor: '#76323F', 
+    borderWidth: 1,
+    borderRadius: 5,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 'auto',
+    marginBottom: 'auto',
+    // margin: 'auto',
+  }
+
+
+})
